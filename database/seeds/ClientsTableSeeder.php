809e18a97b54f654685d0359fbdfcad25f9075e2
jmfcientista <jmfcientista@gmail.com>
2017-03-10 21:43:49 -0300

    atualização delete genero

--- database/seeds/ClientsTableSeeder.php
+++ database/seeds/ClientsTableSeeder.php
@@ -13,26 +13,23 @@ class ClientsTableSeeder extends Seeder
     {
         $client1 = \App\Domain\Clients\Clients::create([
             'name' => 'José Pereira',
-            'gender' => 'Masculino',
             'cpf' => '02343455432',
             'email'=> 'jose@jose.com',
-            'phone'=> 999786775,
+            'phone'=> 85999786775,
         ]);
 
         $client2 = \App\Domain\Clients\Clients::create([
             'name' => 'Paulo José',
-            'gender' => 'Masculino',
             'cpf' => '04378276434',
             'email'=> 'paulo@paulo.com',
-            'phone'=> 34563245,
+            'phone'=> 8534563245,
         ]);
 
         $client3 = \App\Domain\Clients\Clients::create([
             'name' => 'Maria de Nazaré',
-            'gender' => 'Feminino',
             'cpf' => '04583839493',
             'email'=> 'maria@maria.com',
-            'phone'=> 34345656,
+            'phone'=> 8534345656,
         ]);
 
         $client1->save();
